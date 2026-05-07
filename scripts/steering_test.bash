0974f82a7961db903a06708ea2b5551c2564da16
ErykHalicki <erykhalicki0@gmail.com>
2026-05-07 23:01:29 +0200

    steerin

--- scripts/steering_test.bash
+++ scripts/steering_test.bash
@@ -1,3 +1,1 @@
-cd MimicKit
-
-python mimickit/run.py --mode train --num_envs 1024 --engine_config data/engines/newton_engine.yaml --env_config data/envs/amp_steering_env.yaml --agent_config data/agents/amp_task_humanoid_agent.yaml --visualize false --out_dir output/
+python mimickit/run.py --mode train --num_envs 1024 --engine_config data/engines/newton_engine.yaml --env_config data/envs/amp_steering_humanoid_env.yaml --agent_config data/agents/amp_task_humanoid_agent.yaml --visualize false --out_dir output/
